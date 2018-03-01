4d3fc70ea447fad3e75f474b233296f72a0d219d
bystrzano4 <robert.bystrzanowski@gmail.com>
2018-03-01 17:48:25 +0100

    Create ResponseBag.php

---
+++ src/KrzysiekPiasecki/Dotpay/Response/ResponseBag.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace KrzysiekPiasecki\Dotpay\Response;
+
+class ResponseBag 
+{
+    public $id;
+
+    public $operation_number;
+    
+    public $operation_type;
+
+    public $operation_status;
+
+    public $operation_amount;
+
+    public $operation_currency;
+
+    public $operation_withdrawal_amount;
+
+    public $operation_commission_amount;
+
+    public $is_completed;
+
+    public $operation_original_amount;
+
+    public $operation_original_currency;
+
+    public $operation_datetime;
+
+    public $operation_related_number;
+
+    public $control;
+
+    public $desription;
+
+    public $email;
+
+    public $p_info;
+
+    public $p_email;
+
+    public $credit_card_issuer_identification_number;
+
+    public $credit_card_masked_number;
+
+    public $credit_card_brand_codename;
+
+    public $credit_card_brand_code;
+
+    public $credit_card_id;
+
+    public $channel;
+
+    public $channel_country;
+
+    public $geoip_country;
+
+    public $signature;
+}
