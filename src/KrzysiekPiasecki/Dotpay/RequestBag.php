8fd55297efadaa8abcdd59478ca2a947c427034a
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2018-03-02 19:13:26 +0100

    Add RequestBagTest and fix CS

--- src/KrzysiekPiasecki/Dotpay/RequestBag.php
+++ src/KrzysiekPiasecki/Dotpay/RequestBag.php
@@ -11,7 +11,7 @@
 namespace KrzysiekPiasecki\Dotpay;
 
 /**
- * Data structure to represent the request sent to Dotpay
+ * Data structure to represent the request sent to Dotpay.
  */
 class RequestBag
 {
@@ -30,40 +30,40 @@ class RequestBag
     /** @var string Transaction description */
     public $description;
 
-    /** @var string Transaction language **/
+    /** @var string Transaction language */
     public $lang;
 
-    /** @var string Payment channel **/
+    /** @var string Payment channel */
     public $channel;
 
-    /** @var string Force channel **/
+    /** @var string Force channel */
     public $ch_lock;
 
-    /** @var string Ignore last payment channel **/
+    /** @var string Ignore last payment channel */
     public $ignore_last_payment_channel;
 
-    /** @var string Channel groups **/
+    /** @var string Channel groups */
     public $channel_groups;
 
-    /** @var string Target URL after payment **/
+    /** @var string Target URL after payment */
     public $URL;
 
     /** @var string Target URL type */
     public $type;
 
-    /** @var string Back Button display text **/
+    /** @var string Back Button display text */
     public $buttontext;
 
     /** @var string Dotpay rules acceptance flag */
     public $bylaw;
 
-    /** @var string Authorisation by the client to process his personal data **/
+    /** @var string Authorisation by the client to process his personal data */
     public $personal_data;
 
-    /** @var string URL callback **/
+    /** @var string URL callback */
     public $URLC;
 
-    /** @var string Date of expiration of the payment request.  **/
+    /** @var string Date of expiration of the payment request. */
     public $expiration_date;
 
     /** @var string Transaction control number */
@@ -78,7 +78,7 @@ class RequestBag
     /** @var string Client email address */
     public $email;
 
-    /** @var string Client street name*/
+    /** @var string Client street name */
     public $street;
 
     /** @var string Client street number */
