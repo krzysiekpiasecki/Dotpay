7be79a1dfaf858e24569db9d396133ad77d67ef3
bystrzano4 <robert.bystrzanowski@gmail.com>
2018-03-08 18:51:42 +0100

    Create tests for Amount, Blikcode, Buttontext and Id Validators

--- src/KrzysiekPiasecki/Dotpay/Validation/Request/BlikCodeValidator.php
+++ src/KrzysiekPiasecki/Dotpay/Validation/Request/BlikCodeValidator.php
@@ -23,7 +23,7 @@ class BlikCodeValidator extends RegexValidator
     /**
      * Validate against {@see BlikCodeConstraint}.
      *
-     * @param mixed      $value      Validated value
+     * @param mixed $value Validated value
      * @param Constraint $constraint Used constraint
      */
     public function validate($value, Constraint $constraint)
