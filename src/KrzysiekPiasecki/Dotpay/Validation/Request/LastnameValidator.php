181928a65b8900caaf61a32a7aac7a9e42d5afd4
bystrzano4 <robert.bystrzanowski@gmail.com>
2018-03-09 14:18:56 +0100

    Composer fix

--- src/KrzysiekPiasecki/Dotpay/Validation/Request/LastnameValidator.php
+++ src/KrzysiekPiasecki/Dotpay/Validation/Request/LastnameValidator.php
@@ -23,7 +23,7 @@ class LastnameValidator extends RegexValidator
     /**
      * Validate against {@see LastnameConstraint}.
      *
-     * @param mixed $value Validated value
+     * @param mixed      $value      Validated value
      * @param Constraint $constraint Used constraint
      */
     public function validate($value, Constraint $constraint)
