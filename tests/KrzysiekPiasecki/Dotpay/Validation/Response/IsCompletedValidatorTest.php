e9713e898eaa0aee4ead6d0a2c4a8f596a4f7f72
bystrzano4 <robert.bystrzanowski@gmail.com>
2018-03-11 11:47:14 +0100

    Create IsCompletedValidatorTest.php

---
+++ tests/KrzysiekPiasecki/Dotpay/Validation/Response/IsCompletedValidatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Dotpayds project.
+ * (c) Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
+ *
+ * @license   https://opensource.org/licenses/MIT  The MIT License
+ */
+
+namespace KrzysiekPiasecki\Dotpay\Validation\Response;
+
+use KrzysiekPiasecki\Dotpay\Validation\Response\Constraint\IsCompletedConstraint;
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
+
+/**
+ * @coversDefaultClass \KrzysiekPiasecki\Dotpay\Validation\Response\IsCompletedValidator
+ */
+class IsCompletedValidatorTest extends ConstraintValidatorTestCase
+{
+    /**
+     * @covers ::validate()
+     * @dataProvider provideValidValues
+     */
+    public function testValidValue(string $value)
+    {
+        $constraint = new IsCompletedConstraint();
+        $this->validator->validate($value, $constraint);
+        $this->assertNoViolation();
+    }
+
+    /**
+     * @covers ::validate()
+     */
+    public function testInvalidValue()
+    {
+        $this->markTestSkipped('Not implemented yet');
+    }
+
+    /**
+     * @return array
+     */
+    public function provideValidValues(): array
+    {
+        return [
+            ['false'],
+            ['true'],
+        ];
+    }
+
+    protected function createValidator()
+    {
+        return new IsCompletedValidator();
+    }
+}
