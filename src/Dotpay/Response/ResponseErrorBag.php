4f4d7e02a84c777d0cc328305b9c0e2fa5e0e492
bystrzano4 <robert.bystrzanowski@gmail.com>
2018-03-26 23:05:13 +0200

    Create ErrorBag

---
+++ src/Dotpay/Response/ResponseErrorBag.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+* This file is part of Dotpayds project.
+* (c) Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
+*
+* @license   https://opensource.org/licenses/MIT  The MIT License
+*/
+
+declare(strict_types=1);
+
+namespace Dotpay\Response;
+
+/**
+ * Data structure to represent ErrorBag.
+ */
+class ResponseErrorBag
+{
+    /** @var string error code */
+    public $error_code;
+}
