8a02592fef641c8ff5b9e29eb1d883541f90fc32
bystrzano4 <bystrzano4@users.noreply.github.com>
2018-03-27 08:32:59 +0200

    Create ResponseErrorBag (#17)

---
+++ src/Dotpay/Response/Validator/Constraint/ErrorCodeConstraint.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Dotpayds project.
+ * (c) Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
+ *
+ * @license   https://opensource.org/licenses/MIT  The MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Dotpay\Response\Validator\Constraint;
+
+use Dotpay\Response\Validator\ErrorCodeValidator;
+use Symfony\Component\Validator\Constraints\Choice;
+
+/**
+ * Constraint against 'error_code' parameter.
+ *
+ * @see ErrorBag::$error_code Parameter 'error_code'
+ * @Annotation
+ * @Target({"PROPERTY"})
+ */
+class ErrorCodeConstraint extends Choice
+{
+    /** @var string Constraint message */
+    public $message = 'The value {{ error_code }} is not a valid \'error_code\' parameter';
+
+    /**
+     * Available error codes.
+     *
+     * @var array
+     */
+    public static $values = [
+        'PAYMENT_EXPIRED',
+        'UNKNOWN_CHANNEL',
+        'DISABLED_CHANNEL',
+        'UNKNOWN_CURRENCY',
+        'BLOCKED_ACCOUNT',
+        'INACTIVE_SELLER',
+        'AMOUNT_TOO_LOW',
+        'AMOUNT_TOO_HIGH',
+        'BAD_DATA_FORMAT',
+        'REQUIRED_PARAMETERS_NOT_PRESENT',
+        'MULTIMERCHANT_INVALID_ACCOUNT_CONFIGURATION',
+        'MULTIMERCHANT_INSUFFICIENT_AMOUNT',
+        'MULTIMERCHANT_WRONG_CURRENCY',
+        'UNKNOWN_ERROR',
+    ];
+
+    /**
+     * ErrorCodeConstraint constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::$values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy()
+    {
+        return ErrorCodeValidator::class;
+    }
+}
---
+++ tests/Dotpay/Response/Validator/ErrorCodeValidatorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Dotpayds project.
+ * (c) Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
+ *
+ * @license   https://opensource.org/licenses/MIT  The MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Dotpay\Response\Validator;
+
+use Dotpay\Response\Validator\Constraint\ErrorCodeConstraint;
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
+
+/**
+ * @coversDefaultClass \Dotpay\Response\Validator\ErrorCodeValidator
+ */
+class ErrorCodeValidatorTest extends ConstraintValidatorTestCase
+{
+    /**
+     * @covers ::validate()
+     * @dataProvider provideValidValues
+     */
+    public function testValidValue(string $value)
+    {
+        $constraint = new ErrorCodeConstraint();
+        $this->validator->validate($value, $constraint);
+        $this->assertNoViolation();
+    }
+
+    /**
+     * @covers ::validate()
+     */
+    public function testInvalidValue()
+    {
+        $this->markTestSkipped('Not implemented yet');
+    }
+
+    /**
+     * @return array
+     */
+    public function provideValidValues(): array
+    {
+        return [
+            ['PAYMENT_EXPIRED'],
+            ['UNKNOWN_CHANNEL'],
+            ['DISABLED_CHANNEL'],
+            ['UNKNOWN_CURRENCY'],
+            ['BLOCKED_ACCOUNT'],
+            ['INACTIVE_SELLER'],
+            ['AMOUNT_TOO_LOW'],
+            ['AMOUNT_TOO_HIGH'],
+            ['BAD_DATA_FORMAT'],
+            ['REQUIRED_PARAMETERS_NOT_PRESENT'],
+            ['MULTIMERCHANT_INVALID_ACCOUNT_CONFIGURATION']
+            ['MULTIMERCHANT_INSUFFICIENT_AMOUNT'],
+            ['MULTIMERCHANT_WRONG_CURRENCY'],
+            ['UNKNOWN_ERROR'],
+        ];
+    }
+
+    protected function createValidator()
+    {
+        return new ErrorCodeValidator();
+    }
+}
